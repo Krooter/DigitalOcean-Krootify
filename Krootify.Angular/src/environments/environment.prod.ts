35b701d45a31ed65ec42c9a7d85386d092613251
Cristian Voroneanu <cristian.voroneanu@admaris.com>
2022-05-30 20:16:30 +0300

    #12 - Update enviroment prod

--- Krootify.Angular/src/environments/environment.prod.ts
+++ Krootify.Angular/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  openApiUrl: '/',
-  apiUrl: 'api/',
+  openApiUrl: 'https://krootify.azurewebsites.net',
+  apiUrl: 'https://krootify.azurewebsites.net/api/',
   stripePublicKey:
     'pk_test_51Kk8vlDs74tN5fIYe2ayv1YlBGpqE5VMELbTPjpAE9EXAbsv5VUn5YjLGFNMGmwPXATG6Qw23jSgL5yu2pxE4LOn00wj14VU90',
 };
